e3b542ca09219331ad44079534c4ff95b7f790a9
mvs <mvs@buffalo.edu>
2025-08-13 10:39:59 -0400

    flowfun.m modified to use hat functions

--- aperiodic/src/nonlinear-flow-fit/flowfun.m
+++ aperiodic/src/nonlinear-flow-fit/flowfun.m
@@ -1,11 +1,48 @@
 function q = flowfun(xv)
 
-k1 = 1.18e4;
-k2 = 3.2e4;
-delt = 0.25;
+rknots = [-2 -0.25 0.25 2]';
+theta = [-58950 -2950 2950 58950]';
+Phi = hatfun(xv, rknots);
+q = Phi*theta;
 
-q = zeros(size(xv));
+    function Phi = hatfun(xv, rknots)
+        Phi = zeros(length(xv),length(rknots));
+        
+        % left extrapolation
+        r1 = rknots(1);
+        r2 = rknots(2);
+        mask = xv <= r1;
+        h = r2 - r1;
+        Phi(mask,1) = (r2-xv(mask))/h;
+        Phi(mask,2) = (xv(mask)-r1)/h;
 
-q(xv<=delt) = k1*xv(xv<=delt);
-q(xv>delt) = k1*delt + k2*(xv(xv>delt) - delt);
-q(xv<-delt) = -k1*delt + k2*(xv(xv<-delt) + delt);
+        % right extrapolation
+        r1 = rknots(end-1);
+        r2 = rknots(end);
+        mask = xv > r2;
+        h = r2 - r1;
+        Phi(mask,end) = (xv(mask)-r1)/h;
+        Phi(mask,end-1) = (r2-xv(mask))/h;
+
+        % interior points
+        for n = 1:length(rknots)-1
+            r1 = rknots(n);
+            r2 = rknots(n+1);
+            mask = (xv > r1) & (xv <= r2);
+            h = r2 - r1;
+            Phi(mask,n) = (r2 - xv(mask))/h;
+            Phi(mask,n+1) = (xv(mask) - r1)/h;
+        end
+    end
+
+end
+
+% k1 = 1.18e4;
+% k2 = 3.2e4;
+% delt = 0.25;
+
+% q = zeros(size(xv));
+% 
+% q(xv<=delt) = k1*xv(xv<=delt);
+% q(xv>delt) = k1*delt + k2*(xv(xv>delt) - delt);
+% q(xv<-delt) = -k1*delt + k2*(xv(xv<-delt) + delt);
